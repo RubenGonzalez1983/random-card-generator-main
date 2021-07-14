78cf601a46d2136946f4724d2954c6ad77c86609
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-07-14 00:59:44 +0000

    started with random color

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ window.onload = function() {
   var center = document.querySelector("#center");
   var bottom = document.querySelector("#bottom");
 
+  var colorArray = ["#000000", "#F90909"];
   var suiteArray = ["♦", "♥", "♠", "♣"];
   var numberArray = [
     "1",
@@ -30,12 +31,16 @@ window.onload = function() {
 
   var randomSuite = Math.floor(Math.random() * suiteArray.length);
   var randomNumber = Math.floor(Math.random() * numberArray.length);
+  var randomColor = Math.floor(Math.random() * colorArray.length);
+
   var suite = suiteArray[randomSuite];
   var number = numberArray[randomNumber];
+  var color = colorArray[randomColor];
 
   document.getElementById("top").innerHTML = suite;
   document.getElementById("bottom").innerHTML = suite;
   document.getElementById("center").innerHTML = number;
+  //document.getElementById("color").innerHTML = color;
 
   console.log(suite);
 };
