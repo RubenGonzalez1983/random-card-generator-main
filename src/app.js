b03aa56cae3d491d416c4c543d9ac94b46f9ef76
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-07-14 23:04:44 +0000

    added colors

--- src/app.js
+++ src/app.js
@@ -6,10 +6,6 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  var top = document.querySelector("#top");
-  var center = document.querySelector("#center");
-  var bottom = document.querySelector("#bottom");
-
   var colorArray = ["#000000", "#F90909"];
   var suiteArray = ["♦", "♥", "♠", "♣"];
   var numberArray = [
@@ -31,16 +27,20 @@ window.onload = function() {
 
   var randomSuite = Math.floor(Math.random() * suiteArray.length);
   var randomNumber = Math.floor(Math.random() * numberArray.length);
-  var randomColor = Math.floor(Math.random() * colorArray.length);
 
   var suite = suiteArray[randomSuite];
   var number = numberArray[randomNumber];
-  var color = colorArray[randomColor];
+
+  if (suite == "♦" || suite == "♥") {
+    document.getElementById("top").className += " red";
+    document.getElementById("bottom").className += " red";
+    document.getElementById("center").className += " red";
+  }
+  //document.getElementById("top").className += ' class_two'
 
   document.getElementById("top").innerHTML = suite;
   document.getElementById("bottom").innerHTML = suite;
   document.getElementById("center").innerHTML = number;
-  //document.getElementById("color").innerHTML = color;
 
   console.log(suite);
 };
